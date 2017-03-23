feed01100a1a0195c543da01dba88ca43c67b596
ee11cbb19052e40b07aac0ca060c23ee <ee11cbb19052e40b07aac0ca060c23@gmail.com>
2017-03-23 15:14:32 -0400

    resolves #93
    [build] Updated Travis-CI scripts to publish Javadocs to ghpages as part of release process.

---
+++ travis/ghpages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set +x
+set -e
+
+echo '[ghpages] TRAVIS_TAG='$TRAVIS_TAG
+echo '[ghpages] TRAVIS_BRANCH='$TRAVIS_BRANCH
+echo '[ghpages] TRAVIS_PULL_REQUEST='$TRAVIS_PULL_REQUEST
+
+if [[ -n $TRAVIS_TAG ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+
+    GHPAGES_DIR=${HOME}/ghpages
+
+    git clone --quiet --branch=master https://${MCIO_GITHUB_API_KEY}@github.com/missioncommand/missioncommand.github.io.git $GHPAGES_DIR > /dev/null 2>&1
+
+    cd $GHPAGES_DIR
+
+    git config user.name "missioncommand-bot"
+    git config user.email "builds@travis-ci.com"
+
+    echo '[ghpages] Updating Javadocs..'
+    git rm -rfq --ignore-unmatch docs/emp/android/latest
+    mkdir -p docs/emp/android/latest
+
+    cp -Rf ${TRAVIS_BUILD_DIR}/sdk/sdk-api/build/docs/javadoc/* docs/emp/android/latest
+
+    echo '[ghpages] Publishing Javadocs..'
+    git add --force .
+    git commit -m "[travis] Updated emp3-android Javadocs for $TRAVIS_TAG"
+    git push --quiet > /dev/null 2>&1
+fi
