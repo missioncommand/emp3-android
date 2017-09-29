108dcf78796230b8543eb688b9fb1807b910b5a2
Jenifer Cochran <Jenifer.Cochran@rgi-corp.com>
2017-09-29 08:17:42 -0400

    creating a KMZ exporter class #243

---
+++ sdk/sdk-api/src/main/java/mil/emp3/api/utils/kmz/KMZExporterThread.java
@@ -0,0 +1,19 @@
+package mil.emp3.api.utils.kmz;
+
+import mil.emp3.api.interfaces.IEmpExportToStringCallback;
+import mil.emp3.api.interfaces.IMap;
+import mil.emp3.api.utils.kml.KMLExportThread;
+
+/**
+ * Created by jenifer.Cochran on 9/28/2017.
+ *
+ * This is all just a WIP and testing some things out.
+ */
+
+public class KMZExporterThread
+{
+    protected KMZExporterThread(IMap map, boolean extendedData, IEmpExportToStringCallback callback)
+    {
+
+    }
+}
