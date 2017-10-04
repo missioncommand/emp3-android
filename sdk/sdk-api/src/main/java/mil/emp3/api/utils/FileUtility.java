8f84d77f036cc8693596198691a66e9ec7f8c154
Jenifer.Cochran <jenifer.cochran@rgi-corp.local@RGI-000225.rgi-corp.local>
2017-10-04 13:48:48 -0400

    adding comments to FileUtility #243

--- sdk/sdk-api/src/main/java/mil/emp3/api/utils/FileUtility.java
+++ sdk/sdk-api/src/main/java/mil/emp3/api/utils/FileUtility.java
@@ -1,18 +1,30 @@
 package mil.emp3.api.utils;
 
+import android.os.Environment;
+
 import java.io.File;
 
 /**
- * Created by jenifer.cochran@rgi-corp.local on 10/4/17.
+ * File Utility class to perform File and Directory operations.
+ *
+ * @author Jenifer Cochran
  */
 
 public final class FileUtility
 {
+    /**
+     * Prevents instantiation of the class
+     */
     private FileUtility()
     {
 
     }
 
+    /**
+     * Deletes all of the Folder's contents
+     *
+     * @param folder the folder to delete
+     */
     public static void deleteFolder(File folder)
     {
         File[] files = folder.listFiles();
@@ -30,6 +42,47 @@ public final class FileUtility
             }
         }
         folder.delete();
+    }
+
+    /**
+     * Creates the a directory
+     *
+     * @param outputDirectory the location of the directory
+     */
+    public static void createOutputDirectory(String outputDirectory)
+    {
+        File directory = new File(outputDirectory);
+        directory.mkdirs();
+        if(!directory.isDirectory())
+        {
+            throw new IllegalArgumentException(String.format("The temporaryDirectory must be a path to a Directory. %s is not a directory.",
+                                                             outputDirectory));
+        }
+    }
+
+    /**
+     * Checks if external storage is available for read and write
+     * @return true if the external storage is available for read and write,
+     *         false otherwise
+     */
+    public static boolean isExternalStorageWritable()
+    {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+
+    /**
+     * Checks if the location is a directory as
+     * well as if it exists.
+     *
+     * @param directoryPath the location of the directory to check
+     * @return true if is a directory and it exists, false otherwise
+     */
+    public static boolean directoryExists(String directoryPath)
+    {
+        File dir = new File(directoryPath);
+        return dir.exists() && dir.isDirectory();
     }
 
 }
