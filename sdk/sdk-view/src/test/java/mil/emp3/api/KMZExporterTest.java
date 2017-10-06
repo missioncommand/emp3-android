1287e5d7d7fc547b19e7612d7895d3a2be988c34
Jenifer.Cochran <jenifer.cochran@rgi-corp.local@RGI-000225.rgi-corp.local>
2017-10-06 15:34:26 -0400

    adding tests #243

--- sdk/sdk-view/src/test/java/mil/emp3/api/KMZExporterTest.java
+++ sdk/sdk-view/src/test/java/mil/emp3/api/KMZExporterTest.java
@@ -1,8 +1,150 @@
 package mil.emp3.api;
 
+import android.os.Environment;
+import android.util.Xml;
+
+import com.google.common.io.Files;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.File;
+
+import mil.emp3.api.interfaces.IEmpExportToTypeCallBack;
+import mil.emp3.api.utils.FileUtility;
+import mil.emp3.api.utils.kmz.EmpKMZExporter;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.reflect.Whitebox.setInternalState;
+
 /**
  * Created by jenifer.cochran@rgi-corp.local on 10/2/17.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Environment.class, Xml.class})
+public class KMZExporterTest extends TestBaseSingleMap
+{
+    private final static String TAG = KMZExporterTest.class.getName();
+    private static File outputDirectory = Files.createTempDir();
 
-public class KMZExporterTest {
+    @Before
+    public void setUp() throws Exception
+    {
+        super.init();
+        super.setupSingleMap(TAG);
+
+
+        XmlSerializer mockSerializer = PowerMockito.mock(XmlSerializer.class);
+        mockStatic(Xml.class);
+        when(Xml.newSerializer()).thenReturn(mockSerializer);
+
+        if(!outputDirectory.exists())
+        {
+            outputDirectory = Files.createTempDir();
+        }
+
+        mockStatic(Environment.class);
+        setInternalState(Environment.class, "DIRECTORY_PICTURES", "Pictures");
+
+        // Make the Environment class return a mocked external storage directory
+        when(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES))
+                .thenReturn(outputDirectory);
+
+
+    }
+
+    @After
+    public void tearDown()
+    {
+        if(outputDirectory.exists())
+        {
+            FileUtility.deleteFolder(outputDirectory);
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidMapParameter()
+    {
+        final boolean[] processEnded        = {false};
+        String  kmzFileNameWithoutExtension = "TestKmzFileName";
+
+        EmpKMZExporter.exportToKMZ(null,  //invalid input
+                                   false,
+                                   new IEmpExportToTypeCallBack<File>(){
+                                                                           @Override
+                                                                           public void exportSuccess(File exportObject)
+                                                                           {
+                                                                               processEnded[0] = true;
+                                                                           }
+                                                                           @Override
+                                                                           public void exportFailed(Exception Ex)
+                                                                           {
+                                                                               processEnded[0] = true;
+                                                                               Assert.fail(Ex.getMessage());
+                                                                           }
+                                                                       },
+                                   outputDirectory.getAbsolutePath(),
+                                   kmzFileNameWithoutExtension);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidCallbackParameter()
+    {
+        final boolean[] processEnded        = {false};
+        String  kmzFileNameWithoutExtension = "TestKmzFileName";
+
+        EmpKMZExporter.exportToKMZ(this.remoteMap,
+                                   false,
+                                   null,       //invalid input
+                                   outputDirectory.getAbsolutePath(),
+                                   kmzFileNameWithoutExtension);
+    }
+
+    @Test
+    public void exportKmzTest()
+    {
+        String  kmzFileNameWithoutExtension = "TestKmzFileName";
+        final boolean[] processEnded        = {false};
+        final File[]    kmzFile             = new File[1];
+
+        EmpKMZExporter.exportToKMZ(this.remoteMap,
+                                   false,
+                                   new IEmpExportToTypeCallBack<File>(){
+                                                                           @Override
+                                                                           public void exportSuccess(File exportObject)
+                                                                           {
+                                                                               kmzFile[0] = exportObject;
+                                                                               processEnded[0] = true;
+                                                                           }
+
+                                                                           @Override
+                                                                           public void exportFailed(Exception Ex)
+                                                                           {
+                                                                                processEnded[0] = true;
+                                                                                Assert.fail(Ex.getMessage());
+                                                                           }
+                                                                       },
+                                   outputDirectory.getAbsolutePath(),
+                                   kmzFileNameWithoutExtension);
+
+        while(processEnded[0] == false)
+        {
+            //wait until the thread has ended to verify success
+        }
+
+        if(kmzFile[0] != null)
+        {
+            kmzFile[0].delete();
+        }
+    }
 }
--- sdk/sdk-view/src/test/java/mil/emp3/api/mock/MockXmlSerializer.java
+++
@@ -1,9 +0,0 @@
-package mil.emp3.api.mock;
-
-/**
- * Created by jenifer.cochran@rgi-corp.local on 10/6/17.
- */
-
-public class MockXmlSerializer
-{
-}
