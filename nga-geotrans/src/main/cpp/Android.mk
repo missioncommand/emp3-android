56017ef2f45938173ce9671880066edea22a54e6
emp3-coev3 <rajendra.supnekar@rgi-corp.com>
2017-10-13 08:13:41 -0400

    Brought geographicLib changes from branch #240-geotrans-jni

--- nga-geotrans/src/main/cpp/Android.mk
+++
@@ -1,8 +0,0 @@
-
-LOCAL_PATH:= $(call my-dir)
-
-include $(LOCAL_PATH)/GeoTrans.mk
-include $(LOCAL_PATH)/MSPdtcc.mk
-include $(LOCAL_PATH)/MSPCoordinateConversionService.mk
-include $(LOCAL_PATH)/jnimsp_ccs.mk
-
--- nga-geotrans/src/main/cpp/Application.mk
+++
@@ -1,3 +0,0 @@
-#APP_ABI := x86 armeabi-v7a
-APP_ABI := all
-APP_STL := stlport_static
