1ab570b0870b98e192d2f4dacfa9058a31def5b4
Matt Miller <matt.miller@rgi-corp.com>
2017-10-27 13:09:45 -0400

    progress

--- sdk/sdk-view/src/test/java/mil/emp3/api/PolygonTest.java
+++ sdk/sdk-view/src/test/java/mil/emp3/api/PolygonTest.java
@@ -1,41 +1,86 @@
 package mil.emp3.api;
 
-import android.graphics.Bitmap;
 import android.util.Log;
 
+import org.cmapi.primitives.GeoFillStyle;
+import org.cmapi.primitives.GeoLabelStyle;
 import org.cmapi.primitives.GeoPolygon;
 import org.cmapi.primitives.GeoPosition;
-import org.cmapi.primitives.GeoRectangle;
+import org.cmapi.primitives.GeoStrokeStyle;
 import org.cmapi.primitives.IGeoPosition;
+import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.UUID;
 
-import static org.junit.Assert.*;
+import mil.emp3.api.enums.FeatureTypeEnum;
+import mil.emp3.api.utils.ComparisonUtil;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Created by Matt.Miller on 10/16/2017.
  */
-public class PolygonTest extends TestBase{
+public class PolygonTest extends TestBase {
     public static final String TAG = PolygonTest.class.getSimpleName();
-    private Polygon p;
+    private Polygon poly;
     private GeoPolygon gp;
     private ArrayList<IGeoPosition> positions;
 
-    @org.junit.Before
-    public void setUp() throws Exception {
-        p = new Polygon();
+    @Test
+    public void defaultConstructor() throws Exception {
+        poly = new Polygon();
+        ComparisonUtil.validatePolygon(poly,
+                null,
+                new GeoPolygon(),
+                FeatureTypeEnum.GEO_POLYGON,
+                Collections.EMPTY_LIST,
+                Collections.EMPTY_LIST,
+                Collections.EMPTY_LIST,
+                Collections.EMPTY_LIST,
+                null,
+                Collections.EMPTY_LIST,
+                null,
+                new GeoStrokeStyle(),
+                new GeoFillStyle(),
+                new GeoLabelStyle(),
+                false,
+                true,
+                0.0,
+                0.0,
+                null,
+                false,
+                Collections.EMPTY_LIST,
+                false,
+                Collections.EMPTY_LIST,
+                "",
+                new UUID(0,0),
+                null,
+                "",
+                new HashMap());
+    }
+
+    @Test
+    public void geoPolygonConstructor() {
         gp = new GeoPolygon();
-        p = new Polygon(gp);
+        poly = new Polygon(gp);
+    }
+
+    @Test
+    public void positionsConstructor() {
         positions = new ArrayList<IGeoPosition>();
         positions.add(new GeoPosition());
-        p = new Polygon(positions);
+        poly = new Polygon(positions);
     }
 
     @org.junit.Test
     public void invalidConstructors() throws Exception{
         positions = null;
         try {
-            p = new Polygon(positions);
+            poly = new Polygon(positions);
             fail();
         } catch (final IllegalArgumentException e) {
             assertEquals(e.getMessage(), "The position list parameter can NOT be null");
@@ -43,7 +88,7 @@ public class PolygonTest extends TestBase{
         positions = new ArrayList<IGeoPosition>();
         positions.add(null);
         try {
-            p = new Polygon(positions);
+            poly = new Polygon(positions);
             fail();
         } catch (final IllegalArgumentException e) {
             assertEquals(e.getMessage(), "GeoPosition is invalid");
@@ -55,20 +100,17 @@ public class PolygonTest extends TestBase{
     //TODO: Add validation of image files
     @org.junit.Test
     public void setPatternFillImage() throws Exception {
-        p = new Polygon();
-        p.setPatternFillImage(p.getPatternFillImage());
+        poly = new Polygon();
+        poly.setPatternFillImage(poly.getPatternFillImage());
     }
 
-    @org.junit.Test
-    public void getPatternFillImage() throws Exception {
-    }
 
     @org.junit.Test
     public void stringTest() throws Exception {
-        p = new Polygon();
-        Log.i(TAG, p.toString());
-        p.setPosition(new GeoPosition());
-        Log.i(TAG, p.toString());
+        poly = new Polygon();
+        Log.i(TAG, poly.toString());
+        poly.setPosition(new GeoPosition());
+        Log.i(TAG, poly.toString());
     }
 
 }
