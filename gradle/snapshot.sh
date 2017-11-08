c51bd72b9d1e70ad7f4ba95f6d543541bc554cfd
James Rummel <james.rummel@rgi-corp.com>
2017-11-08 18:10:17 -0500

    Changing scripts back to point to development. Should be good to go now.

--- gradle/snapshot.sh
+++ gradle/snapshot.sh
@@ -4,7 +4,7 @@ echo '[snapshot] TRAVIS_BRANCH='$TRAVIS_BRANCH
 echo '[snapshot] TRAVIS_PULL_REQUEST='$TRAVIS_PULL_REQUEST
 
 # we only publish for specific branches and if NOT a PR
-if [[ "$TRAVIS_BRANCH" == "fixing-deployments" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+if [[ "$TRAVIS_BRANCH" == "development" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     ./gradlew artifactoryPublish --stacktrace
     exit $?
 fi
