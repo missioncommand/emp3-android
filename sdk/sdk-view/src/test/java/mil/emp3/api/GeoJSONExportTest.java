e38380bf4e817cf6fa5e7a64e4820019e542fc77
rsupnekar <rsupnekar@users.noreply.github.com>
2017-12-13 16:04:17 -0500

    Fixed GeoJsonExporter and added GeoJSONExportTest (#413)
    * Fixed GeoJsonExporter and added GeoJSONExportTest
    
    * Fixed comment

---
+++ sdk/sdk-view/src/test/java/mil/emp3/api/GeoJSONExportTest.java
@@ -0,0 +1,459 @@
+package mil.emp3.api;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.robolectric.util.Logger;
+
+import mil.emp3.api.abstracts.Feature;
+import mil.emp3.api.interfaces.IEmpExportToStringCallback;
+import mil.emp3.json.geoJson.GeoJsonCaller;
+
+import static mil.emp3.api.utils.ComparisonUtils.compareFeatureToPath;
+import static mil.emp3.api.utils.ComparisonUtils.compareFeatureToPolygon;
+import static mil.emp3.api.utils.ComparisonUtils.comparePath;
+import static mil.emp3.api.utils.ComparisonUtils.comparePoint;
+import static mil.emp3.api.utils.ComparisonUtils.comparePolygon;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class GeoJSONExportTest extends TestBaseSingleMap{
+        private final static String TAG = mil.emp3.api.GeoJSONExportTest.class.getName();
+        private static int count = 0;
+
+        @Before
+        public void setup() throws Exception{
+            super.init();
+            super.setupSingleMap(TAG);
+        }
+
+        @Test
+        public void exportPoint() throws Exception
+        {
+
+            final Overlay overlay       = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Point feature         = KMLExportTest.addPoint(overlay);
+            final boolean[] resultFound = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    feature,
+                    false,
+                    new IEmpExportToStringCallback()
+                    {
+                        @Override
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            try
+                            {
+                                final GeoJSON geoJSON = new GeoJSON(geoJSONStr);
+                                final Point importedPoint = (Point) geoJSON.getFeatureList().get(0);
+                                comparePoint(importedPoint, feature);
+                            } catch (final Exception e){
+                                fail(e.getMessage());
+                            }
+                        }
+
+                        @Override
+                        public void exportFailed(Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+        @Test
+        public void exportOverlay() throws Exception {
+            this.remoteMap.clearContainer();
+            final Overlay overlay       = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Point feature         = KMLExportTest.addPoint(overlay);
+            final boolean[] resultFound = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    overlay,
+                    true,
+                    new IEmpExportToStringCallback()
+                    {
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            try
+                            {
+                                final GeoJSON geoJSON = new GeoJSON(geoJSONStr);
+                                final Point importedPoint = (Point) geoJSON.getFeatureList().get(0);
+                                comparePoint(importedPoint, feature);
+                            } catch (final Exception e){
+                                fail(e.getMessage());
+                            }
+                        }
+
+                        @Override
+                        public void exportFailed(Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+        @Test
+        public void exportMap() throws Exception {
+            this.remoteMap.clearContainer();
+            final Overlay overlay         = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Point feature           = KMLExportTest.addPoint(overlay);
+            final boolean[] resultFound   = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    true,
+                    new IEmpExportToStringCallback()
+                    {
+                        @Override
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            try
+                            {
+                                final GeoJSON geoJSON = new GeoJSON(geoJSONStr);
+                                final Point importedPoint = (Point) geoJSON.getFeatureList().get(0);
+                                comparePoint(importedPoint, feature);
+                            } catch (final Exception e){
+                                fail(e.getMessage());
+                            }
+                        }
+
+                        @Override
+                        public void exportFailed(Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+        @Test
+        public void exportPolygon() throws Exception
+        {
+            this.remoteMap.clearContainer();
+            final Overlay overlay       = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Polygon feature       = KMLExportTest.addPolygon(overlay);
+            final boolean[] resultFound = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    feature,
+                    true,
+                    new IEmpExportToStringCallback()
+                    {
+                        @Override
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            try
+                            {
+                                final GeoJSON geoJSON = new GeoJSON(geoJSONStr);
+                                final Polygon importedPolygon = (Polygon) geoJSON.getFeatureList().get(0);
+                                comparePolygon(importedPolygon, feature);
+                            } catch (final Exception e){
+                                fail(e.getMessage());
+                            }
+                        }
+
+                        @Override
+                        public void exportFailed(Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+        private void parseGeoJSON(String featureType, String geoJSONStr, Feature feature){
+            try {
+                final GeoJSON geoJSON = new GeoJSON(geoJSONStr);
+                //Circles do not exist in KML so they are represented by paths
+                for (int i = 0; i < geoJSON.getFeatureList().size(); i++) {
+                    Object obj = geoJSON.getFeatureList().get(i);
+                    if (obj instanceof Path) {
+                        Logger.info(featureType + " is rendered as Path");
+                        Logger.info(feature.toString());
+                        Logger.info(obj.toString());
+                        compareFeatureToPath(feature, (Path) obj);
+                    } else if (obj instanceof Polygon) {
+                        Logger.info(featureType + " is rendered as Polygon");
+                        Logger.info(feature.toString());
+                        Logger.info(obj.toString());
+                        compareFeatureToPolygon(feature, (Polygon) obj);
+                    } else if (obj instanceof Point && featureType.equals("Text")) {
+                        String str1 = ((Text) feature).getText();
+                        String str2 = ((Text) obj).getText();
+                        Assert.assertEquals(str1, str2);
+                    } else {
+                        Logger.error(featureType + " is rendered as " + obj.getClass().getSimpleName());
+                    }
+                }
+            } catch (Exception e) {
+                fail(e.getMessage());
+            }
+        }
+
+        @Test
+        public void exportCircle() throws Exception
+        {
+            this.remoteMap.clearContainer();
+            final Overlay overlay       = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Circle feature        = KMLExportTest.addCircle(overlay);
+            final boolean[] resultFound = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    feature,
+                    true,
+                    new IEmpExportToStringCallback()
+                    {
+                        @Override
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            parseGeoJSON("Circle", geoJSONStr, feature);
+                        }
+
+                        @Override
+                        public void exportFailed(final Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+        @Test
+        public void exportEllipse() throws Exception
+        {
+            this.remoteMap.clearContainer();
+            final Overlay overlay       = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Ellipse feature       = KMLExportTest.addEllipse(overlay);
+            final boolean[] resultFound = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    feature,
+                    true,
+                    new IEmpExportToStringCallback()
+                    {
+                        @Override
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            parseGeoJSON("Ellipse", geoJSONStr, feature);
+                        }
+                        @Override
+                        public void exportFailed(Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+        @Test
+        public void exportSquare() throws Exception
+        {
+            this.remoteMap.clearContainer();
+            final Overlay overlay       = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Square feature        = KMLExportTest.addSquare(overlay);
+            final boolean[] resultFound = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    feature,
+                    true,
+                    new IEmpExportToStringCallback()
+                    {
+                        @Override
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            parseGeoJSON("Square", geoJSONStr, feature);
+                        }
+
+                        @Override
+                        public void exportFailed(Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+        @Test
+        public void exportRectangle() throws Exception
+        {
+            this.remoteMap.clearContainer();
+            final Overlay overlay       = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Rectangle feature     = KMLExportTest.addRectangle(overlay);
+            final boolean[] resultFound = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    feature,
+                    true,
+                    new IEmpExportToStringCallback()
+                    {
+                        @Override
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            parseGeoJSON("Rectangle", geoJSONStr, feature);
+                        }
+
+                        @Override
+                        public void exportFailed(Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+        @Test
+        public void exportText() throws Exception
+        {
+            this.remoteMap.clearContainer();
+            final Overlay overlay       = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Text feature          = KMLExportTest.addText(overlay);
+            final boolean[] resultFound = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    feature,
+                    true,
+                    new IEmpExportToStringCallback()
+                    {
+                        @Override
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            System.out.println(geoJSONStr);
+                            parseGeoJSON("Text", geoJSONStr, feature);
+                        }
+
+                        @Override
+                        public void exportFailed(Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+        @Test
+        public void exportPath() throws Exception
+        {
+            this.remoteMap.clearContainer();
+            final Overlay overlay       = (Overlay) KMLExportTest.addOverlayToMap(this.remoteMap);
+            final Path feature          = KMLExportTest.addPath(overlay);
+            final boolean[] resultFound = {false};
+
+            GeoJsonCaller.exportToString(this.remoteMap,
+                    feature,
+                    true,
+                    new IEmpExportToStringCallback()
+                    {
+                        @Override
+                        public void exportSuccess(final String geoJSONStr)
+                        {
+                            resultFound[0] = true;
+                            try
+                            {
+                                final GeoJSON geoJSON = new GeoJSON(geoJSONStr);
+                                final Path importedPath = (Path) geoJSON.getFeatureList().get(0);
+                                comparePath(feature, importedPath);
+                            } catch (final Exception e){
+                                fail(e.getMessage());
+                            }
+                        }
+
+                        @Override
+                        public void exportFailed(Exception ex)
+                        {
+                            resultFound[0] = true;
+                            fail(ex.getMessage());
+                        }
+                    });
+
+            while(!resultFound[0]) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        }
+
+    }
