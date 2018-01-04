57649c11b02c672de3f049ab93c9e49e06c54d81
Jenifer Cochran <jenifer.cochran@rgi-corp.com>
2018-01-04 14:47:57 -0500

    commenting out tests that randomly fail travis build-this time the right way

--- sdk/sdk-view/src/test/java/mil/emp3/api/KMLExportTest.java
+++ sdk/sdk-view/src/test/java/mil/emp3/api/KMLExportTest.java
@@ -38,62 +38,65 @@ import static org.junit.Assert.fail;
 public class KMLExportTest extends TestBaseSingleMap{
     private final static String TAG = KMLExportTest.class.getName();
     private static int count = 0;
-//TODO: this is failing in Travis builds as referenced in issue #442.  This will be fixed when issue #442 is completed
-//    @Before
-//    public void setup() throws Exception{
-//        super.init();
-//        super.setupSingleMap(TAG);
-//    }
+
+    @Before
+    public void setup() throws Exception
+    {
+        super.init();
+        super.setupSingleMap(TAG);
+    }
+
+    @Test
+    public void exportPoint() throws Exception
+    {
+
+        final Overlay overlay       = (Overlay) addOverlayToMap(this.remoteMap);
+        final Point feature         = addPoint(overlay);
+        final boolean[] resultFound = {false};
+
+        EmpKMLExporter.exportToString(this.remoteMap,
+                                      feature,
+                                      false,
+                                      new IEmpExportToStringCallback()
+                                      {
+                                        @Override
+                                        public void exportSuccess(final String kmlString)
+                                        {
+                                            try
+                                            {
+                                                final KML kmlFeature = new KML(kmlString);
+                                                final Point importedPoint = (Point) kmlFeature.getFeatureList().get(0);
+                                                comparePoint(importedPoint, feature);
+                                                resultFound[0] = true;
+                                            } catch (final Exception e){
+                                                resultFound[0] = true;
+                                                fail(e.getMessage());
+                                            }
+                                            resultFound[0] = true;
+                                        }
+
+                                        @Override
+                                        public void exportFailed(Exception ex)
+                                        {
+                                            resultFound[0] = true;
+                                            fail(ex.getMessage());
+                                        }
+                                      });
+
+        int i = 0;
+        while(!resultFound[0] && ++i <= 10) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ie) {
+
+            }
+        }
+        if (i >= 10) {
+            fail("No result in half second for exportPoint");
+        }
+    }
+//TODO: these tests are failing in Travis builds as referenced in issue #442.  This will be fixed when issue #442 is completed
 //
-//    @Test
-//    public void exportPoint() throws Exception
-//    {
-//
-//        final Overlay overlay       = (Overlay) addOverlayToMap(this.remoteMap);
-//        final Point feature         = addPoint(overlay);
-//        final boolean[] resultFound = {false};
-//
-//        EmpKMLExporter.exportToString(this.remoteMap,
-//                                      feature,
-//                                      false,
-//                                      new IEmpExportToStringCallback()
-//                                      {
-//                                        @Override
-//                                        public void exportSuccess(final String kmlString)
-//                                        {
-//                                            try
-//                                            {
-//                                                final KML kmlFeature = new KML(kmlString);
-//                                                final Point importedPoint = (Point) kmlFeature.getFeatureList().get(0);
-//                                                comparePoint(importedPoint, feature);
-//                                                resultFound[0] = true;
-//                                            } catch (final Exception e){
-//                                                resultFound[0] = true;
-//                                                fail(e.getMessage());
-//                                            }
-//                                            resultFound[0] = true;
-//                                        }
-//
-//                                        @Override
-//                                        public void exportFailed(Exception ex)
-//                                        {
-//                                            resultFound[0] = true;
-//                                            fail(ex.getMessage());
-//                                        }
-//                                      });
-//
-//        int i = 0;
-//        while(!resultFound[0] && ++i <= 10) {
-//            try {
-//                Thread.sleep(50);
-//            } catch (InterruptedException ie) {
-//
-//            }
-//        }
-//        if (i >= 10) {
-//            fail("No result in half second for exportPoint");
-//        }
-//    }
 //
 //    @Test
 //    public void exportOverlay() throws Exception {
@@ -532,136 +535,136 @@ public class KMLExportTest extends TestBaseSingleMap{
 //        }
 //    }
 //
-//    static IOverlay addOverlayToMap(final IMap map) throws EMP_Exception
-//    {
-//        final Overlay overlay = new Overlay();
-//        overlay.setName("Test Overlay " + count++);
-//        map.addOverlay(overlay, true);
-//        return overlay;
-//    }
-//
-//    static Point addPoint(final IOverlay overlay) throws Exception
-//    {
-//        final Point oPoint = new Point();
-//        oPoint.setIconScale(10);
-//        oPoint.setIconURI("https://127.0.0.1");
-//        oPoint.setIconStyle(new GeoIconStyle());
-//        addFeatureStyles(oPoint);
-//        overlay.addFeature(oPoint, true);
-//        return oPoint;
-//    }
-//
-//    static Text addText(final IOverlay overlay) throws Exception
-//    {
-//        final Text oText = new Text();
-//        oText.setRotationAngle(40);
-//        oText.setText("Test String");
-//        addFeatureStyles(oText);
-//        overlay.addFeature(oText, true);
-//        return oText;
-//    }
-//
-//    static Circle addCircle(final IOverlay overlay) throws Exception
-//    {
-//        final Circle oCircle = new Circle();
-//        oCircle.setName("Test Circle");
-//        oCircle.setRadius(15.5);
-//        addFeatureStyles(oCircle);
-//        overlay.addFeature(oCircle, true);
-//        return oCircle;
-//    }
-//
-//    static Ellipse addEllipse(final IOverlay overlay) throws Exception
-//    {
-//        final Ellipse oEllipse = new Ellipse();
-//        oEllipse.setSemiMinor(15.5);
-//        oEllipse.setSemiMajor(14.5);
-//        addFeatureStyles(oEllipse);
-//        overlay.addFeature(oEllipse, true);
-//        return oEllipse;
-//    }
-//
-//    static Square addSquare(final IOverlay overlay) throws Exception
-//    {
-//        final Square oSquare = new Square();
-//        oSquare.setWidth(15.5);
-//        addFeatureStyles(oSquare);
-//        overlay.addFeature(oSquare, true);
-//        return oSquare;
-//    }
-//
-//    static Rectangle addRectangle(final IOverlay overlay) throws Exception
-//    {
-//        final Rectangle oRectangle = new Rectangle();
-//        oRectangle.setWidth(15.5);
-//        oRectangle.setHeight(14.5);
-//        addFeatureStyles(oRectangle);
-//        overlay.addFeature(oRectangle, true);
-//        return oRectangle;
-//    }
-//
-//    static Polygon addPolygon(final IOverlay overlay) throws Exception
-//    {
-//        final Polygon oPolygon = new Polygon();
-//        addMPFeatureStyles(oPolygon);
-//        overlay.addFeature(oPolygon, true);
-//        return oPolygon;
-//    }
-//
-//    static Path addPath(final IOverlay overlay) throws Exception
-//    {
-//        final Path oPath = new Path();
-//        addFeatureStyles(oPath);
-//        overlay.addFeature(oPath, true);
-//        return oPath;
-//    }
-//
-//    static GeoPosition getRandomLocation() throws EMP_Exception {
-//        final GeoPosition location = new GeoPosition();
-//        location.setLatitude(getRandomValueBetween(-90.0,90.0));
-//        location.setLongitude(getRandomValueBetween(-180.0, 180.0));
-//        return location;
-//    }
-//
-//    static double getRandomValueBetween(final double low, final double high)
-//    {
-//        return low + (high - low) * new Random().nextDouble();
-//    }
-//
-//    static void addFeatureStyles(final Feature feature) throws Exception{
-//        feature.setPosition(getRandomLocation());
-//        feature.setName("Test Feature");
-//        //feature.setAzimuth(50);
-//        feature.setExtrude(true);
-//        feature.setDescription("This is a test feature");
-//        feature.setFillStyle(new GeoFillStyle());
-//        feature.setLabelStyle(new GeoLabelStyle());
-//        feature.setStrokeStyle(new GeoStrokeStyle());
-//        feature.setAltitudeMode(IGeoAltitudeMode.AltitudeMode.CLAMP_TO_GROUND);
-//        feature.setBuffer(25.0);
-//        feature.setPathType(IGeoRenderable.PathType.LINEAR);
-//        feature.setTessellate(false);
-//        feature.setReadOnly(false);
-//    }
-//
-//    static void addMPFeatureStyles(final Feature feature) throws Exception{
-//        feature.getPositions().clear();
-//        for (int i = 0; i < 4; i++) {
-//            feature.getPositions().add(getRandomLocation());
-//        }
-//        feature.setName("Test Feature");
-//        //feature.setAzimuth(50);
-//        feature.setExtrude(true);
-//        feature.setDescription("This is a test feature");
-//        feature.setFillStyle(new GeoFillStyle());
-//        feature.setLabelStyle(new GeoLabelStyle());
-//        feature.setStrokeStyle(new GeoStrokeStyle());
-//        feature.setAltitudeMode(IGeoAltitudeMode.AltitudeMode.CLAMP_TO_GROUND);
-//        feature.setBuffer(25.0);
-//        feature.setPathType(IGeoRenderable.PathType.LINEAR);
-//        feature.setTessellate(false);
-//        feature.setReadOnly(false);
-//    }
+    static IOverlay addOverlayToMap(final IMap map) throws EMP_Exception
+    {
+        final Overlay overlay = new Overlay();
+        overlay.setName("Test Overlay " + count++);
+        map.addOverlay(overlay, true);
+        return overlay;
+    }
+
+    static Point addPoint(final IOverlay overlay) throws Exception
+    {
+        final Point oPoint = new Point();
+        oPoint.setIconScale(10);
+        oPoint.setIconURI("https://127.0.0.1");
+        oPoint.setIconStyle(new GeoIconStyle());
+        addFeatureStyles(oPoint);
+        overlay.addFeature(oPoint, true);
+        return oPoint;
+    }
+
+    static Text addText(final IOverlay overlay) throws Exception
+    {
+        final Text oText = new Text();
+        oText.setRotationAngle(40);
+        oText.setText("Test String");
+        addFeatureStyles(oText);
+        overlay.addFeature(oText, true);
+        return oText;
+    }
+
+    static Circle addCircle(final IOverlay overlay) throws Exception
+    {
+        final Circle oCircle = new Circle();
+        oCircle.setName("Test Circle");
+        oCircle.setRadius(15.5);
+        addFeatureStyles(oCircle);
+        overlay.addFeature(oCircle, true);
+        return oCircle;
+    }
+
+    static Ellipse addEllipse(final IOverlay overlay) throws Exception
+    {
+        final Ellipse oEllipse = new Ellipse();
+        oEllipse.setSemiMinor(15.5);
+        oEllipse.setSemiMajor(14.5);
+        addFeatureStyles(oEllipse);
+        overlay.addFeature(oEllipse, true);
+        return oEllipse;
+    }
+
+    static Square addSquare(final IOverlay overlay) throws Exception
+    {
+        final Square oSquare = new Square();
+        oSquare.setWidth(15.5);
+        addFeatureStyles(oSquare);
+        overlay.addFeature(oSquare, true);
+        return oSquare;
+    }
+
+    static Rectangle addRectangle(final IOverlay overlay) throws Exception
+    {
+        final Rectangle oRectangle = new Rectangle();
+        oRectangle.setWidth(15.5);
+        oRectangle.setHeight(14.5);
+        addFeatureStyles(oRectangle);
+        overlay.addFeature(oRectangle, true);
+        return oRectangle;
+    }
+
+    static Polygon addPolygon(final IOverlay overlay) throws Exception
+    {
+        final Polygon oPolygon = new Polygon();
+        addMPFeatureStyles(oPolygon);
+        overlay.addFeature(oPolygon, true);
+        return oPolygon;
+    }
+
+    static Path addPath(final IOverlay overlay) throws Exception
+    {
+        final Path oPath = new Path();
+        addFeatureStyles(oPath);
+        overlay.addFeature(oPath, true);
+        return oPath;
+    }
+
+    static GeoPosition getRandomLocation() throws EMP_Exception {
+        final GeoPosition location = new GeoPosition();
+        location.setLatitude(getRandomValueBetween(-90.0,90.0));
+        location.setLongitude(getRandomValueBetween(-180.0, 180.0));
+        return location;
+    }
+
+    static double getRandomValueBetween(final double low, final double high)
+    {
+        return low + (high - low) * new Random().nextDouble();
+    }
+
+    static void addFeatureStyles(final Feature feature) throws Exception{
+        feature.setPosition(getRandomLocation());
+        feature.setName("Test Feature");
+        //feature.setAzimuth(50);
+        feature.setExtrude(true);
+        feature.setDescription("This is a test feature");
+        feature.setFillStyle(new GeoFillStyle());
+        feature.setLabelStyle(new GeoLabelStyle());
+        feature.setStrokeStyle(new GeoStrokeStyle());
+        feature.setAltitudeMode(IGeoAltitudeMode.AltitudeMode.CLAMP_TO_GROUND);
+        feature.setBuffer(25.0);
+        feature.setPathType(IGeoRenderable.PathType.LINEAR);
+        feature.setTessellate(false);
+        feature.setReadOnly(false);
+    }
+
+    static void addMPFeatureStyles(final Feature feature) throws Exception{
+        feature.getPositions().clear();
+        for (int i = 0; i < 4; i++) {
+            feature.getPositions().add(getRandomLocation());
+        }
+        feature.setName("Test Feature");
+        //feature.setAzimuth(50);
+        feature.setExtrude(true);
+        feature.setDescription("This is a test feature");
+        feature.setFillStyle(new GeoFillStyle());
+        feature.setLabelStyle(new GeoLabelStyle());
+        feature.setStrokeStyle(new GeoStrokeStyle());
+        feature.setAltitudeMode(IGeoAltitudeMode.AltitudeMode.CLAMP_TO_GROUND);
+        feature.setBuffer(25.0);
+        feature.setPathType(IGeoRenderable.PathType.LINEAR);
+        feature.setTessellate(false);
+        feature.setReadOnly(false);
+    }
 
 
 }
